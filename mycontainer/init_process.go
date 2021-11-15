5b03b4257288ab4b618b8251aa18f5b7b16906f8
魏巍 <wewin11235@163.com>
2021-11-15 21:40:14 +0800

    fix mount err

--- mycontainer/init_process.go
+++ mycontainer/init_process.go
@@ -11,6 +11,8 @@ import (
 func InitProcess(cmd string, args []string) error {
 	log.Info("command: %s", cmd)
 
+	syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
+
 	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
 
